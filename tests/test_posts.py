237b1a8598dd26e67bcb4f9b30dece28414798e7
edagne18 <156398879+edagne18@users.noreply.github.com>
2026-03-13 05:11:21 -0700

    Add schema validation to post tests

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,4 +1,5 @@
 import pytest
+from app import schemas
 
 
 @pytest.mark.parametrize(
@@ -39,41 +40,51 @@ def test_unauthorized_user_cannot_access_posts(client, method, url, body):
 def test_get_all_posts(authorized_client, test_posts):
     res = authorized_client.get("/posts/")
 
+    posts = [schemas.PostOut(**post) for post in res.json()]
+
     assert res.status_code == 200
-    assert len(res.json()) == len(test_posts)
-    assert res.json()[0]["Post"]["title"] == test_posts[0].title
-    assert res.json()[0]["votes"] == 0
+    assert len(posts) == len(test_posts)
+    assert posts[0].Post.title == test_posts[0].title
+    assert posts[0].votes == 0
 
 
 def test_get_posts_limit(authorized_client, test_posts):
     res = authorized_client.get("/posts/?limit=2")
 
+    posts = [schemas.PostOut(**post) for post in res.json()]
+
     assert res.status_code == 200
-    assert len(res.json()) == 2
+    assert len(posts) == 2
 
 
 def test_get_posts_search(authorized_client, test_posts):
     res = authorized_client.get("/posts/?search=first")
 
+    posts = [schemas.PostOut(**post) for post in res.json()]
+
     assert res.status_code == 200
-    assert len(res.json()) == 1
-    assert res.json()[0]["Post"]["title"] == "first title"
+    assert len(posts) == 1
+    assert posts[0].Post.title == "first title"
 
 
 def test_get_posts_limit_and_skip(authorized_client, test_posts):
     res = authorized_client.get("/posts/?limit=2&skip=1")
 
+    posts = [schemas.PostOut(**post) for post in res.json()]
+
     assert res.status_code == 200
-    assert len(res.json()) == 2
+    assert len(posts) == 2
 
 
 def test_get_single_post(authorized_client, test_posts):
     res = authorized_client.get(f"/posts/{test_posts[0].id}")
 
+    post = schemas.PostOut(**res.json())
+
     assert res.status_code == 200
-    assert res.json()["Post"]["id"] == test_posts[0].id
-    assert res.json()["Post"]["title"] == test_posts[0].title
-    assert res.json()["votes"] == 0
+    assert post.Post.id == test_posts[0].id
+    assert post.Post.title == test_posts[0].title
+    assert post.votes == 0
 
 
 def test_get_single_post_not_found(authorized_client):
@@ -86,9 +97,11 @@ def test_get_single_post_not_found(authorized_client):
 def test_get_other_users_post(authorized_client, test_posts):
     res = authorized_client.get(f"/posts/{test_posts[2].id}")
 
+    post = schemas.PostOut(**res.json())
+
     assert res.status_code == 200
-    assert res.json()["Post"]["id"] == test_posts[2].id
-    assert res.json()["Post"]["owner_id"] == test_posts[2].owner_id
+    assert post.Post.id == test_posts[2].id
+    assert post.Post.owner_id == test_posts[2].owner_id
 
 
 def test_create_post(authorized_client, test_user):
@@ -100,11 +113,13 @@ def test_create_post(authorized_client, test_user):
 
     res = authorized_client.post("/posts/", json=post_data)
 
+    created_post = schemas.Post(**res.json())
+
     assert res.status_code == 201
-    assert res.json()["title"] == post_data["title"]
-    assert res.json()["content"] == post_data["content"]
-    assert res.json()["published"] == post_data["published"]
-    assert res.json()["owner_id"] == test_user["id"]
+    assert created_post.title == post_data["title"]
+    assert created_post.content == post_data["content"]
+    assert created_post.published == post_data["published"]
+    assert created_post.owner_id == test_user["id"]
 
 
 def test_delete_own_post(authorized_client, test_posts):
@@ -139,11 +154,13 @@ def test_update_own_post(authorized_client, test_posts):
 
     res = authorized_client.put(f"/posts/{test_posts[0].id}", json=data)
 
+    updated_post = schemas.Post(**res.json())
+
     assert res.status_code == 200
-    assert res.json()["id"] == test_posts[0].id
-    assert res.json()["title"] == data["title"]
-    assert res.json()["content"] == data["content"]
-    assert res.json()["published"] == data["published"]
+    assert updated_post.id == test_posts[0].id
+    assert updated_post.title == data["title"]
+    assert updated_post.content == data["content"]
+    assert updated_post.published == data["published"]
 
 
 def test_update_post_not_found(authorized_client):
